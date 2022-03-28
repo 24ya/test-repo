749edff072f3be0320e87f531d957824fe3300c4
Song Ga Hyeon <81414118+SongGaHyeon@users.noreply.github.com>
2022-03-29 01:53:05 +0900

    Update 제출용 코드.py

--- 제출용 코드.py
+++ 제출용 코드.py
@@ -160,6 +160,6 @@ if winner=='':
         winner = '%s' % p2
         print('The winner is %s!' % winner)
     else:
-        print('WOW! Draw!')
+        print('WOW! Draw!!!!!!')
 else:
     print('The winner is %s!'%winner)
